04f4ed45eb398531fe6134ced2e12149109a412c
Swaymos <theswayamos@gmail.com>
2024-02-26 11:58:14 +0100

    my first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Get Information On Any Car Brand You Find Interesting
+        </h1>
+
+        <p className="hero__subtitle">Get all the Information</p>
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
