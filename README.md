29a887ac9544b01a8d80cc3beb533d3c1d4058f7
Swaymos <theswayamos@gmail.com>
2024-02-27 11:03:15 +0100

    Made updates to markdown

--- README.md
+++ README.md
@@ -1,34 +1,27 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+<div align="">
 
-## Getting Started
+# <a name="introduction">Auto Mode</a>
 
-First, run the development server:
+   <div align="">
+  Get useful information on any car brand and model. View images and some interesting facts about any car brand.
+    </div>
+</div>
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+## <a name="tech-stack">⚙️ Technologies Used In Building Auto Mode</a>
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- React.js
+- TypeScript
+- Tailwind Css
+- Ninja Image API
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## <a name="features">🔋 Features</a>
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+👉 **Car Features**: Get the information of the car brand and model you wish to learn more about.
 
-## Learn More
+👉 **Car Media**: View the image of your favourite car brand or model, look at multiple angles of the car .
 
-To learn more about Next.js, take a look at the following resources:
+## <a name="features">🔋 Would You Love To Check Out Auto Mode?</a>
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+Follow the link to view the live project. Thanks for watching.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+#
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,23 +1,27 @@
 "use client";
 
-import { useState, Fragment } from 'react';
-import Image from 'next/image';
-import { Combobox, Transition } from '@headlessui/react'
+import { useState, Fragment } from "react";
+import Image from "next/image";
+import { Combobox, Transition } from "@headlessui/react";
 
-import { manufacturers } from '@/constants';
-import { SearchManufacturerProps } from '@/types';
+import { manufacturers } from "@/constants";
+import { SearchManufacturerProps } from "@/types";
 
-const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) => {
-  const [query, setQuery] = useState('');
+const SearchManufacturer = ({
+  selected,
+  setSelected,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
 
-  const filteredManufacturers = 
-    query === "" 
-      ? manufacturers 
-      : manufacturers.filter((item) => (
-        item.toLowerCase()
-        .replace(/\s+/g, "")
-        .includes(query.toLowerCase().replace(/\s+/g, "")
-      )))
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
     <div className="search-manufacturer">
@@ -33,7 +37,7 @@ const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) 
             />
           </Combobox.Button>
 
-          <Combobox.Input 
+          <Combobox.Input
             className="search-manufacturer__input"
             placeholder="Volkswagen"
             displayValue={(manufacturer: string) => manufacturer}
@@ -45,45 +49,45 @@ const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) 
             leave="transition ease-in duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            afterLeave={() => setQuery('')}
+            afterLeave={() => setQuery("")}
           >
             <Combobox.Options>
               {filteredManufacturers.map((item) => (
-                  <Combobox.Option
-                    key={item}
-                    className={({ active}) => `
-                    relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' :'text-gray-900'}
+                <Combobox.Option
+                  key={item}
+                  className={({ active }) => `
+                    relative search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }
                     `}
-                    value={item}
-                  >
-                    {({ selected, active }) => (
-                      <>
+                  value={item}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {item}
+                      </span>
+                      {selected ? (
                         <span
-                          className={`block truncate ${
-                            selected ? 'font-medium' : 'font-normal'
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
                           }`}
-                        >
-                          {item}
-                        </span>
-                        {selected ? (
-                          <span
-                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                              active ? 'text-white' : 'text-teal-600'
-                            }`}
-                          >
-                          </span>
-                        ) : null}
-                      </>
-                    )}
-                  </Combobox.Option>
-                )
-              )}
+                        ></span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))}
             </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
     </div>
-  )
-}
+  );
+};
 
-export default SearchManufacturer
+export default SearchManufacturer;
